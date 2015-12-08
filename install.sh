3b1cf1603f76f2aa287d1056d337cecec08100ec
Gregory Higginson <GHigginson@appneta.com>
2015-12-07 17:13:58 -0800

    Import of vim profile

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ else
   echo "if [ -f ~/.bash_profile ]; then source ~/.bash_profile fi" >> ~/.bashrc
 fi
 
+if [ -e ~/.vimrc ]; then
+  echo "VIM Profile already installed."  
+else
+  echo "Installing VIM profile..."
+  ln -s $PWD/profile/vimrc ~/.vimrc
+fi

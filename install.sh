a1ef96c935ad5a317c23be4491ad7fa519eab270
Gregory Higginson <GHigginson@appneta.com>
2016-06-18 12:43:06 -0700

    Cleaned up install process

--- install.sh
+++ install.sh
@@ -2,34 +2,30 @@
 
 PWD=`pwd`
 
+replacify() {
+  SOURCE=$1
+  TARGET=$2
+  if [ -L "$TARGET" ]; then
+    echo "Skipping $TARGET..."  
+  else
+    if [ -e "$TARGET" ]; then
+      echo "Backing up $TARGET..."  
+      mv -f "$TARGET" "${TARGET}.bak"
+    fi
+    echo "Installing $TARGET..."
+    mkdir -p `dirname "$TARGET"`
+    ln -s "$PWD/$SOURCE" "$TARGET"
+  fi
+}
+
 if [ ! -e ~/.bashrc ]; then
   echo "No ~/.bashrc found, cannot install bash_profile.";
-elif [ -e ~/.bash_profile ]; then
-  echo "Profile already installed."  
 else
-  echo "Installing profile..."
-  ln -s $PWD/profile/bash_profile ~/.bash_profile
   echo "if [ -f ~/.bash_profile ]; then source ~/.bash_profile; fi" >> ~/.bashrc
 fi
 
-if [ -e ~/.vimrc ]; then
-  echo "VIM Profile already installed."  
-else
-  echo "Installing VIM profile..."
-  ln -s $PWD/profile/vimrc ~/.vimrc
-fi
-
-if [ -e ~/.gitconfig ]; then
-  echo "GIT config already installed."  
-else
-  echo "Installing GIT config..."
-  ln -s $PWD/profile/gitconfig ~/.gitconfig
-fi
-
-if [ -e ~/scripts ]; then
-  echo "Scripts already installed".;
-else
-  echo "Installing scripts..."
-  ln -s $PWD/scripts ~/scripts
-fi
-
+replacify profile/bash_profile ~/.bash_profile
+replacify profile/vimrc ~/.vimrc
+replacify profile/gitconfig ~/.gitconfig
+replacify profile/atom/config.cson ~/.atom/config.cson
+replacify bin ~/bin 
